4de97424d68dcf8385475b30fdad1e7f3cd5f406
rern <rernrern@gmail.com>
2017-07-13 18:07:32 +0700

    Update title.sh

--- title.sh
+++ title.sh
@@ -110,12 +110,14 @@ usage() {
 	lcolor -
 }
 printcolor() {
+	echo
 	for i in {0..255} ; do
 	    printf "\x1b[48;5;%sm%3d\e[0m " "$i" "$i"
 	    if (( i == 15 )) || (( i > 15 )) && (( (i-15) % 6 == 0 )); then
 		printf "\n";
 	    fi
 	done
+	echo
 }
 
 [[ $1 == -h || $1 == --help || $1 == -? ]] && usage
