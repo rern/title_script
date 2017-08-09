bd8122aa3d72db348df3a9c9a64ed1d062efef3e
rern <rernrern@gmail.com>
2017-08-09 08:28:04 +0700

    Update title.sh

--- title.sh
+++ title.sh
@@ -124,7 +124,7 @@ colorchart() {
 }
 
 yesno() { # $1 = header string; $2 = input or <enter> = ''
-	title "$info $1"
+	echo -e "\n$info $1"
 	echo -e '  \e[0;36m0\e[m No'
 	echo -e '  \e[0;36m1\e[m Yes'
 	echo
@@ -133,7 +133,7 @@ yesno() { # $1 = header string; $2 = input or <enter> = ''
 	echo
 }
 setpwd() { # return $pwd1
-	echo -e "$info Password: "
+	echo -e "\n$info Password: "
 	read -s pwd1
 	echo
 	echo 'Retype password: '
